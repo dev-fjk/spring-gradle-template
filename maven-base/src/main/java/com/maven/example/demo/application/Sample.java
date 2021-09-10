ba18a08635a6af9ea93ec2e07bac92762b12e12b
dev-fjk <fjk.develop@gmail.com>
2021-09-11 01:28:08 +0900

    テスト用のサンプルクラス作成

---
+++ maven-base/src/main/java/com/maven/example/demo/application/Sample.java
@@ -0,0 +1,20 @@
+package com.maven.example.demo.application;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class Sample {
+
+	public int sum(int a, int b) {
+		log.info("sum START");
+
+		int result = a + b;
+
+		log.info("result : {}", result);
+		log.info("sum END");
+		return result;
+	}
+}
