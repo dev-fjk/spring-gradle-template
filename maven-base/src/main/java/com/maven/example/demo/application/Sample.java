c7f8b89a002bfeaea81c1d1e6e52e863f2abe1f2
dev-fjk <fjk.develop@gmail.com>
2021-09-11 15:55:41 +0900

    spock使用時のサンプルコード設定

--- maven-base/src/main/java/com/maven/example/demo/application/Sample.java
+++ maven-base/src/main/java/com/maven/example/demo/application/Sample.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class Sample {
-
+    
     public int sum(int a, int b) {
         log.info("sum START");
-        log.info("intellij test");
 
         int result = a + b;
+
         log.info("result : {}", result);
         log.info("sum END");
         return result;
